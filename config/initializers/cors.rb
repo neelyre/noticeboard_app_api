ce260ff2caa4b3163a24ef057cc229f8b4ff431c
Richard Neely <richardneely@Richards-Mac-Pro.local>
2017-09-05 20:35:54 -0400

    added EXTRAS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,4 +13,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :update, :delete, :options, :head]
   end
+
+  allow do
+    origins 'localhost:3001', 'https://neelynoticeboard.herokuapp.com/'
+
+    resource '*',
+    headers: :any,
+    methods: [:get, :post, :update, :delete, :options, :head]
+
+  end
 end
